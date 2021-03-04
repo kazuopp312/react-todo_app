de587d7d7f9b51326bf8828a61325e40ab9c5a1b
kazuopp312 <kazuopp312@gmail.com>
2021-03-05 04:18:53 +0900

    Done

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   useEffect(() => {
     //this code fires when the app.js loads
     db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
-      setTodos(snapshot.docs.map(doc => doc.data().todo))
+      setTodos(snapshot.docs.map(doc => ({id: doc.id ,todo: doc.data().todo})))
     })
   }, []);
 
@@ -46,7 +46,7 @@ function App() {
 
       <ul>
         {todos.map(todo => (
-          <Todo text={todo} />
+          <Todo todo={todo} />
         ))}
       </ul>
     </div>
--- src/Todo.js
+++ src/Todo.js
@@ -1,16 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Todo.css';
-import { List, Avatar, ImageIcon,ListItemAvatar, ListItem, ListItemText } from '@material-ui/core';
+import { List, Avatar, ImageIcon,ListItemAvatar, ListItem, ListItemText, Button, Modal } from '@material-ui/core';
+import db from './firebase'
+import { makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function Todo(props) {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [input, setInput] = useState();
+
+  const handleOpen = () => {
+    setOpen(true)
+  };
+
+  const updateTodo = () => {
+    // update the todo with the new input text
+
+    db.collection('todos').doc(props.todo.id).set({
+      todo: input
+    }, { merge: true })
+    setOpen(false);
+  }
+
   return (
-    <List className="todo_list">
+    <>
+    <Modal
+      open={open}
+      onClose={e => setOpen(false)}
+    >
+      <div className = {classes.paper}>
+        <h1>I am a modal</h1>
+        <input placeholder={props.todo.todo} value={input} onChange={event => setInput(event.target.value)} />
+        <Button onClick={updateTodo}>Update Todo</Button>
+      </div>
+    </Modal>
+    <List>
       <ListItem>
         <ListItemAvatar>
         </ListItemAvatar>
-        <ListItemText primary={props.text} secondary='Deadline' />
+        <ListItemText primary={props.todo.todo} secondary='Deadline' />
       </ListItem>
+      <button onClick = {e => setOpen(true)}>Edit</button>
+      <Button onClick={event => db.collection('todos').doc(props.todo.id).delete()}>DELETE ME</Button>
     </List>
+    </>
   )
 }
 
